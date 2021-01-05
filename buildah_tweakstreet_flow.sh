f4c177aad5da5d2d0514a8b306bcdbe428521718
uwegeercken <uwegeercken@web.de>
2021-01-05 18:36:36 +0100

    more description text

--- buildah_tweakstreet_flow.sh
+++ buildah_tweakstreet_flow.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
+#
+# Script to create a OCI compliant image from the buildah_tweakstreet base image. The script copies all files required by the controlflow or dataflow into the relevant folders and sets the working directory to the /home/tweakstreet/flows folder.
+#
+# Following folders are available:
+# - the Tweakstreet ETL tool root folder: /opt/tweakstreet
+# - JDBC drivers need to be copied to: /home/tweakstreet/.tweakstreet/drivers
+# - dataflows, control flows, modules, etc: /home/tweakstreet/flows
+# - data files required by the flows are copied to: /home/tweakstreet/ 
+#
+# The /opt/tweakstreet/bin folder where the shell script to run flows - engine.sh - is located, is available on the path.
+# 
+# example to run the image:
+#   podman run --rm -it tweakstreet-test01:0.1 engine.sh dataflow-test.dfl
+#   or
+#   docker run --rm -it tweakstreet-test01:0.1 engine.sh dataflow-test.dfl
+#
+# last update: uwe.geercken@web.de - 2021-01-05
+#
 
+# tweakstreet home folder in the base image
 tweakstreet_home="/home/tweakstreet"
 
+# registry to pull the base image from
 image_registry="silent1:8083"
 image_registry_group="silent1:8082"
 
@@ -18,17 +38,19 @@ image_author="uwe.geercken@web.de"
 # name of working container
 working_container="${image_name}-working-container"
 
-# local folders
+# local folders, from where the source files are copied
 flows_folder="flows"
 data_folder="data"
 drivers_folder="drivers"
 
-# container folders (drivers for JDBC drivers)
+# container folders (drivers folder is for JDBC drivers)
 tweakstreet_flows="${tweakstreet_home}/flows"
 tweakstreet_data="${tweakstreet_home}/data"
 tweakstreet_drivers="${tweakstreet_home}/.tweakstreet/drivers"
 
 # start of build
+
+# create the working container
 container=$(buildah --name "${working_container}" from ${base_image_tag})
 
 # copy required files
@@ -40,6 +62,9 @@ buildah copy $container "${drivers_folder}/" "${tweakstreet_drivers}"
 buildah config --author "${image_author}" $container
 buildah config --workingdir "${tweakstreet_flows}" $container
 
+# commit container, create image
 buildah commit $container "${image_name}:${image_version}"
+
+# remove working container
 buildah rm $container
 
